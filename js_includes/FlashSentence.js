d35bd5f0ebd7d019bbba94eb78617813ae654f25
Galen Soria <gnsoria@hush.com>
2014-03-05 14:34:24 -0800

    adding ibex stuff

---
+++ js_includes/FlashSentence.js
@@ -0,0 +1,46 @@
+/* This software is licensed under a BSD license; see the LICENSE file for details. */
+
+define_ibex_controller({
+name: "FlashSentence",
+
+jqueryWidget: {
+    _init: function () {
+        this.cssPrefix = this.options._cssPrefix;
+        this.finishedCallback = this.options._finishedCallback;
+        this.utils = this.options._utils;
+
+        if (typeof(this.options.s) != "string")
+            this.sentenceDom = htmlCodeToDOM(this.options.s)
+        else
+            this.sentenceDom = $("<div>").text(this.options.s);
+        
+        this.timeout = dget(this.options, "timeout", 2000);
+
+        this.sentenceDescType = dget(this.options, "sentenceDescType", "literal");
+        assert(this.sentenceDescType == "literal", "Bad value for 'sentenceDescType' option of FlashSentence controller ('md5' no longer supported).");
+        this.sentenceMD5 = csv_url_encode(this.options.s.html ? this.options.s.html : this.options.s+'');
+
+        this.element.addClass(this.cssPrefix + "flashed-sentence");
+        this.element.append(this.sentenceDom);
+
+        if (this.timeout) {
+            var t = this;
+            this.utils.setTimeout(function() {
+                t.finishedCallback([[["Sentence (or sentence MD5)", t.sentenceMD5]]]);
+            }, this.timeout);
+        }
+        else {
+            // Give results without actually finishing.
+            if (this.utils.setResults)
+                this.utils.setResults([[["Sentence (or sentence MD5)", this.sentenceMD5]]]);
+        }
+    }
+},
+
+properties: {
+    obligatory: ["s"],
+    htmlDescription: function (opts) {
+        return $(document.createElement("div")).text(opts.s)[0];
+    }
+}
+});
